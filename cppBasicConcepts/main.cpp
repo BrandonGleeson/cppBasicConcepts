878bbc841438f74a9b6c686dfe2dc8e8f9b0b084
Brandon Gleeson <brandon.gleeson@yahoo.com>
2020-08-09 18:00:34 -0600

    added a line of code from the Ryzen machine

--- cppBasicConcepts/main.cpp
+++ cppBasicConcepts/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Hello World!" << endl;
     cout << "This line was added from laptop" << endl;
     cout << "This line was also added from laptop" << endl;
+    cout << "later, this line was added from the Ryzen" << endl;
     return 0;
 }
