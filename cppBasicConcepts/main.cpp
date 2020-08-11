e6fccb6108d045ba4c2cb1cbb5c0cdf34101b195
BrandonGleeson <brandon.gleeson@yahoo.com>
2020-08-11 05:00:59 -0600

    updated code from 20200810

--- cppBasicConcepts/main.cpp
+++ cppBasicConcepts/main.cpp
@@ -8,5 +8,17 @@ int main()
     cout << "This line was added from laptop" << endl;
     cout << "This line was also added from laptop" << endl;
     cout << "later, this line was added from the Ryzen" << endl;
+    cout << "the new line character can be used to make a line break \n";
+    cout << "this line should be below the one above" << endl;
+    cout << "two new-line characters makes a blank line \n\n" << endl;
+    //this is how you make a single-line command
+    /* This is how you start a multi-line, block comment
+     * note this line is still being commented even though I went to a new line
+     * next line will show the end of the comment block
+     */
+    cout << "we are not beyond the comment sections" << endl;
+
+
+
     return 0;
 }
